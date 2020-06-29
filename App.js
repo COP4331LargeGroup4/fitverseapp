6085c9102183cdda773d613bd55870cf4fc1e806
jkraftchick <54610324+jkraftchick@users.noreply.github.com>
2020-06-29 13:33:34 -0400

    material ui test

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-material-ui';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button primary text="Primary" />
       <StatusBar style="auto" />
     </View>
   );
