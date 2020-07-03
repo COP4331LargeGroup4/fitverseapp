df9ea5aacb6da5f9020bd508f317133953fc3b52
jkraftchick <54610324+jkraftchick@users.noreply.github.com>
2020-07-02 22:58:23 -0400

    wip stuff even more

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, SafeAreaView, View, Image, Dimensions } from 'react-native';
-import { BottomNavigation } from 'react-native-paper';
-import SvgUri from 'react-native-svg-uri';
+import { BottomNavigation, Button, TextInput } from 'react-native-paper';
 
 
 //import * as mdiIcon from '@mdi/react' // can i change the iconprovder to this? or add mdiAccountCowboyHat to react-native-icon-provider or whatever its called
@@ -46,7 +45,7 @@ const ProfileRoute = () =>
 		<Profile />
 	</View >;
 
-const App = () => {
+const Dashboard = () => {
 
 	const [index, setIndex] = React.useState(0);
 	const [routes] = React.useState([
@@ -64,13 +63,11 @@ const App = () => {
 	});
 
 	return (
-		<SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-			<BottomNavigation
-				navigationState={{ index, routes }}
-				onIndexChange={setIndex}
-				renderScene={renderScene}
-			/>
-		</SafeAreaView>
+		<BottomNavigation
+			navigationState={{ index, routes }}
+			onIndexChange={setIndex}
+			renderScene={renderScene}
+		/>
 	);
 };
 
@@ -84,5 +81,125 @@ const styles = StyleSheet.create({
 		justifyContent: 'center'
 	}
 });
+
+const WelcomePage = () => {
+	const [pageState, setPageState] = useState('signin');
+
+	const [email, setEmail] = useState(null);
+	const [password, setPassword] = useState(null);
+
+	const [firstname, setFirstname] = useState(null);
+	const [lastname, setLastname] = useState(null);
+
+	const ValidateEmail = (email) => {
+		console.log(email);
+		setEmail(email);
+	}
+
+	return (
+		<>
+		<Logo 
+			width='100%'
+			height='10%'
+			style={styles.container}
+		/>
+		<View style={{flexDirection:'row', justifyContent:'space-evenly', marginTop:10}}>
+			<Button 
+				style={{width:'40%'}}
+				mode={pageState == 'signin' ? "contained" : "outlined"}
+				onPress={() => setPageState('signin')}
+			>
+				Log in
+			</Button>
+			<Button 
+				style={{width:'40%'}}
+				mode={pageState != 'signin' ? "contained" : "outlined"}
+				onPress={() => setPageState('signup')}
+			>
+				Sign up
+			</Button>
+		</View>
+		{pageState == 'signin' ?
+			<View>
+				<Text style={{textAlign:'center'}}>Log in to your fitverse account</Text>
+				<TextInput
+					label="Email Address"
+					value={email}
+					onChangeText={ValidateEmail}
+					mode='outlined'
+				/>
+				<TextInput
+					label="Password"
+					value={password}
+					onChangeText={password => setPassword(password)}
+					secureTextEntry={true}
+					mode='outlined' // could implement staying logged in after this
+				/>
+				<Button
+					mode='contained'
+				>
+					Sign in
+				</Button>
+				<View>
+					<Text>Forgot password?</Text>
+				</View>
+			</View>
+		:
+		<View>
+			<Text style={{textAlign:'center'}}>Sign up for a fitverse account</Text>
+			<View style={{flexDirection:'row'}}>
+				<TextInput
+					label="Email Address"
+					value={email}
+					onChangeText={ValidateEmail}
+					mode='outlined'
+					style={{width:'50%', justifyContent:'center'}}
+				/>
+				<TextInput
+					label="Password"
+					value={password}
+					onChangeText={password => setPassword(password)}
+					secureTextEntry={true}
+					mode='outlined' // could implement staying logged in after this
+					style={{width:'50%'}}
+				/>
+			</View>
+			<TextInput
+				label="Email Address"
+				value={email}
+				onChangeText={ValidateEmail}
+				mode='outlined'
+			/>
+			<TextInput
+				label="Password"
+				value={password}
+				onChangeText={password => setPassword(password)}
+				secureTextEntry={true}
+				mode='outlined' // could implement staying logged in after this
+			/>
+			<Button
+				mode='contained'
+			>
+				Sign in
+			</Button>
+			<View>
+				<Text>Forgot password?</Text>
+			</View>
+		</View>
+		}
+		</>
+	);
+}
+
+const App = () => {
+	const [userID, setUserID] = useState(-1);
+
+
+	return (
+		<SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+			{userID <= -1 ? <WelcomePage /> : <Dashboard />}
+		</SafeAreaView>
+	)
+}
 
 export default App;
